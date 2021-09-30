2369f7d3443b49705405a50c5db9c54d0a3b81ee
JayJay458 <qiujiajun0430@gmail.com>
2021-09-29 21:45:07 -0400

    fix to accept multi parameter

--- assignment2.js
+++ assignment2.js
@@ -13,7 +13,7 @@ Array.prototype.myFilter = function(operation) {
     let returnValue=[];
     for(let i=0;i<this.length; i++)
     {
-        if(operation(this[i]))
+        if(operation(this[i],i,this))
         {
             returnValue.push(this[i]);
         }
@@ -66,9 +66,17 @@ Object.grabValues = function() {
 
 };
 
+//Test 
+
 let myArray=[1,2,3,4,5,6,7,8]
 
 let printOut = myArray.filter(number => number<6 );
 console.log(printOut);
 printOut = myArray.myFilter(number => number<6 );
+console.log(printOut);
+
+myArray=[0,1,2,3,4,5,6,7,8]
+printOut = myArray.filter((number,i )=> number===i);
+console.log(printOut);
+printOut = myArray.myFilter((number,i )=> number===i );
 console.log(printOut);
