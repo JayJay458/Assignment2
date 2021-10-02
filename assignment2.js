1772712370ac7d90343efcda90fc16104f2ff960
JayJay458 <qiujiajun0430@gmail.com>
2021-10-02 17:36:02 -0400

    implementing function and test

--- assignment2.js
+++ assignment2.js
@@ -29,7 +29,17 @@ Array.prototype.myReduce = function() {
 };
 
 // INCLUDES //
-Array.prototype.myIncludes = function() {
+Array.prototype.myIncludes = function(searchFor,fromIndex=0) {
+
+    for(let i =fromIndex;i<this.length;i++)
+    {
+        if(this[i]===searchFor)
+        {
+            return true;
+        }
+
+    }
+    return false;
 
 };
 
@@ -57,3 +67,15 @@ Object.grabKeys = function() {
 Object.grabValues = function() {
 
 };
+
+//test
+
+let myArray=[1,2,3,4,5,6,7]
+console.log(myArray.includes(6));
+console.log(myArray.myIncludes(6));
+console.log(myArray.includes(12));
+console.log(myArray.myIncludes(12));
+console.log(myArray.includes(2,3));
+console.log(myArray.myIncludes(2,3));
+console.log(myArray.includes(5,3));
+console.log(myArray.myIncludes(5,3));
