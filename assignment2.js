31dcbf802b5866ccbac903bba759fe993e6d73d5
JayJay458 <qiujiajun0430@gmail.com>
2021-10-05 19:13:49 -0400

    merge two file into one

--- assignment2.js
+++ assignment2.js
@@ -1,5 +1,4 @@
 // FOR EACH //
-
 Array.prototype.myEach = function(operation) {
 
     for(let i=0;i<this.length;i++)
@@ -12,8 +11,13 @@ Array.prototype.myEach = function(operation) {
 };
 
 // MAP //
-Array.prototype.myMap = function() {
-
+Array.prototype.myMap = function(map_function) {
+    const temp_array = [];
+    for(let i=0; i<this.length; i++)
+    {
+        temp_array[i] = map_function(this[i]);
+    }
+    return temp_array;
 };
 
 
@@ -32,7 +36,6 @@ Array.prototype.mySome = function(operation)
 
     return false;
 };
-
 
 // FILTER //
 Array.prototype.myFilter = function(operation) {
@@ -66,12 +69,24 @@ Array.prototype.myEvery = function(operation) {
 };
 
 // REDUCE //
-Array.prototype.myReduce = function() {
+Array.prototype.myReduce = function(reduce_function, init_value){
+    if(typeof init_value === "undefined")
+    {
+        let prev_value = this[0];
+        for(let i=1; i<this.length; i++){
+            prev_value = reduce_function(prev_value, this[i]);
+        }
+        return prev_value;
+    }
+    let temp_value = init_value;
+    for(let i=0; i<this.length; i++){
+        temp_value = reduce_function(temp_value, this[i]);
+    }
+    return temp_value;
 
 };
 
 // INCLUDES //
-
 Array.prototype.myIncludes = function(searchFor,fromIndex=0) {
 
     for(let i =fromIndex;i<this.length;i++)
@@ -88,17 +103,42 @@ Array.prototype.myIncludes = function(searchFor,fromIndex=0) {
 };
 
 // INDEXOF //
-Array.prototype.myIndexOf = function() {
-
+Array.prototype.myIndexOf = function(find_obj, start_from = 0) {
+    let index = -1;
+    for(let i = start_from; i<this.length; i++)
+    {
+        if(this[i] === find_obj)
+        {
+            index = i;
+            return index;
+        }
+    }
+    return index;
 };
 
 // PUSH //
-Array.prototype.myPush = function() {
-
+Array.prototype.myPush = function(...push_obj) {
+    let size = this.length;
+    for(let x of push_obj)
+    {
+        this[size] = x;
+        size++;
+    }
+    return size;
 };
 
 // LASTINDEXOF //
-Array.prototype.myLastIndexOf = function() {
+Array.prototype.myLastIndexOf = function(find_obj, start_from = (this.length-1) ) {
+    let index = -1;
+    for(let i = start_from; i>=0; i--)
+    {
+        if(this[i] === find_obj)
+        {
+            index = i;
+            return index;
+        }
+    }
+    return index;
 
 };
 
@@ -128,7 +168,3 @@ Object.grabValues = function(object) {
     }
     return temp_values;
 };
-
-
-
-
